b729605fcca3c2d3c7ba164254e81b2e09227f89
Zachary Cutlip <uid000@gmail.com>
2020-12-04 16:34:03 -0800

    refactor main

--- mock_op/mock_op.py
+++ mock_op/mock_op.py
@@ -1,13 +1,14 @@
+import sys
 from .config.mopconfig import MOPConfig
-from .op_cmds import GetCommand
+from .op_cmds import CommandFactory
 
 
-def mock_op_main(argv):
-    config = MOPConfig(argv)
-    get_cmd = GetCommand(config)
+def mock_op_main():
+    config = MOPConfig(sys.argv[1:])
+    cmd = CommandFactory.command_object(config)
+    resp: bytes = cmd.response()
+    sys.stdout.write(resp)
 
 
 if __name__ == "__main__":
-    import sys
-    args = sys.argv[1:]
-    mock_op_main(args)
+    mock_op_main()
