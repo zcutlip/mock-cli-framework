3c72c6316f429ffb7ca0d5c1a33477652b8e3e06
Zachary Cutlip <uid000@gmail.com>
2021-02-01 12:12:50 -0800

    utility functions to convert arg list to/from a string

---
+++ mock_op/argv_conversion.py
@@ -0,0 +1,23 @@
+from typing import List, AnyStr
+
+
+DEFAULT_SEP = "|"
+
+
+def argv_to_string(argv: List, args_to_pop=0, sep=DEFAULT_SEP):
+    popped_args = []
+    for i in range(0, args_to_pop):
+        popped = argv.pop(0)
+        popped_args.append(popped)
+
+    arg_str = ""
+    for arg in argv:
+        arg_str += f"{arg}{sep}"
+    arg_str.rstrip(sep)
+    return arg_str
+
+
+def argv_from_string(arg_str: AnyStr, popped_args=[], sep=DEFAULT_SEP):
+    argv = list(popped_args)
+    split_args = arg_str.split(sep)
+    argv.extend(split_args)
