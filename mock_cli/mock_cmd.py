1bca932de3bd655ecd14451c089114359efc4864
Zachary Cutlip <uid000@gmail.com>
2022-09-15 20:57:31 -0700

    add method to get response directory

--- mock_cli/mock_cmd.py
+++ mock_cli/mock_cmd.py
@@ -1,11 +1,32 @@
-import sys
 import os
+import sys
+from pathlib import Path
+
+from .mock_cmd_state import MockCMDStateNoDirectoryException
 from .responses import ResponseDirectory
+
+
+class MockCommandResponseDirException(Exception):
+    pass
 
 
 class MockCommand:
     def __init__(self, responsedir_json_file):
         self.response_directory = ResponseDirectory(responsedir_json_file)
+
+    def _get_response_directory(self, response_directory):
+        if response_directory is None:
+            response_directory = self._mock_cmd_state.response_directory_path()
+
+        elif isinstance(response_directory, (str, Path)):
+            response_directory = ResponseDirectory(response_directory)
+        elif isinstance(response_directory, ResponseDirectory):
+            pass
+
+        if response_directory is None:
+            raise MockCMDStateNoDirectoryException(
+                "No response directory provided")
+        return response_directory
 
     def _write_binary(self, output_handle, data):
         with os.fdopen(output_handle.fileno(), "wb", closefd=False) as fd:
