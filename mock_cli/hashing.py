9e1a4dc16f13285aca83b8463d5d93f4f72a04d9
Zachary Cutlip <uid000@gmail.com>
2023-02-17 13:47:14 -0800

    ignore empty string input when hashing

--- mock_cli/hashing.py
+++ mock_cli/hashing.py
@@ -4,7 +4,8 @@ from typing import Optional, Union
 
 def digest_input(input: Optional[Union[str, bytes]]) -> Optional[str]:
     digest = None
-    if input is not None:
+    # ignore input if None or if empty string
+    if input:
         if isinstance(input, str):
             input = input.encode()
         hash = hashlib.md5(input)
