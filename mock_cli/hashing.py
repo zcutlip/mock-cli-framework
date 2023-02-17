e7828bf220695d98e7401197c003e926ad4c7097
Zachary Cutlip <uid000@gmail.com>
2023-02-17 13:20:05 -0800

    add module for hashing input

---
+++ mock_cli/hashing.py
@@ -0,0 +1,12 @@
+import hashlib
+from typing import Optional, Union
+
+
+def digest_input(input: Optional[Union[str, bytes]]) -> Optional[str]:
+    digest = None
+    if input is not None:
+        if isinstance(input, str):
+            input = input.encode()
+        hash = hashlib.md5(input)
+        digest = hash.hexdigest()
+    return digest
