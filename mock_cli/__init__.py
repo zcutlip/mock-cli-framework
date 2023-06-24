2ee5a2cb4895cd18df72453b4c8a6690811c5cbd
Zachary Cutlip <uid000@gmail.com>
2023-06-23 17:03:03 -0700

    add mock_cmd_state clases to package exports

--- mock_cli/__init__.py
+++ mock_cli/__init__.py
@@ -1,6 +1,10 @@
 from .__about__ import __summary__, __title__, __version__  # noqa: F401
 from .about import MockCLIAbout  # noqa: F401
 from .mock_cmd import MockCommand  # noqa: F401
+from .mock_cmd_state import (  # noqa: F401
+    MockCMDNewStateConfig,
+    MockCMDStateConfig
+)
 from .responses import (  # noqa: F401
     CommandInvocation,
     CommandResponse,
