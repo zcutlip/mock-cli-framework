403b77f1359f849490787fba709e1e9a68dfde85
Zachary Cutlip <uid000@gmail.com>
2023-10-06 13:16:08 -0700

    add typing info and docstring to ActualPath

--- mock_cli/path.py
+++ mock_cli/path.py
@@ -1,6 +1,7 @@
 import os
-from pathlib import Path, _windows_flavour, _posix_flavour
 from abc import abstractclassmethod
+from pathlib import Path, _posix_flavour, _windows_flavour
+from typing import Optional, Union
 
 
 class AbstractPath(Path):
@@ -24,7 +25,26 @@ class AbstractPath(Path):
 
 
 class ActualPath(AbstractPath):
-    def __new__(cls, dirname, fname=None, create=False):
+    def __new__(cls, dirname: Union[str, Path], fname: Optional[Union[str, Path]] = None, create: Optional[bool] = False):
+        """
+        Create a fully resolved absolute path object from a path name and optionally a filename.
+        Optionally create the full path on the filesystem during initialization.
+
+        Parameters
+        ----------
+        dirname : Union[str, Path]
+            Main directory path. Will be parent of fname if provided
+        fname : Union[str, Path], optional
+            The name of a file to append to the directory path
+        create : Optional[bool], optional
+            Create the fully qualified directory path on the filesystem.
+            If a filename is provided, the file will be touched on the filesystem, creating an empty file if none exists
+
+        Returns
+        -------
+        ActualPath
+            The fully resolved absolute path object
+        """
         dirname = os.path.expanduser(dirname)
         dirname = os.path.abspath(dirname)
         dirname = os.path.realpath(dirname)
